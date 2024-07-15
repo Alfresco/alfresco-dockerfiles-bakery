2c78b22768fd3e2991b821f46c301c13b36f5d16
pmacius <pawel.maciusiak@hyland.com>
2024-07-15 13:36:24 +0200

    test

--- scripts/fetch-artifact.sh
+++ scripts/fetch-artifact.sh
@@ -7,7 +7,10 @@ DEFAULT_ARTIFACT_NAME=alfresco-content-services-distribution
 ARTIFACT_VERSION=$2
 DEFAULT_ARTIFACT_VERSION=23.2.1
 
-GROUP_ID=$3
+EXTENSION=$3
+DEFAULT_EXTENSION=zip
+
+GROUP_ID=$4
 DEFAULT_GROUP_ID=org.alfresco
 
 if [ -z "$ARTIFACT_NAME" ]; then
@@ -16,6 +19,10 @@ fi
 
 if [ -z "$ARTIFACT_VERSION" ]; then
     ARTIFACT_VERSION=$DEFAULT_ARTIFACT_VERSION
+fi
+
+if [ -z "$EXTENSION" ]; then
+    EXTENSION=$DEFAULT_EXTENSION
 fi
 
 if [ -z "$GROUP_ID" ]; then
@@ -27,4 +34,4 @@ GROUP_ID_AS_PATH=$(echo "$GROUP_ID" | tr . /)
 
 echo "Downloading $GROUP_ID:$ARTIFACT_NAME $ARTIFACT_VERSION from Nexus"
 wget --user "$NEXUS_USERNAME:$NEXUS_PASSWORD" \
-    "https://nexus.alfresco.com/nexus/service/local/repositories/enterprise-releases/content/$GROUP_ID_AS_PATH/$ARTIFACT_NAME/${ARTIFACT_VERSION}/$ARTIFACT_NAME-${ARTIFACT_VERSION}.zip"
+    "https://nexus.alfresco.com/nexus/service/local/repositories/enterprise-releases/content/$GROUP_ID_AS_PATH/$ARTIFACT_NAME/${ARTIFACT_VERSION}/$ARTIFACT_NAME-${ARTIFACT_VERSION}.${EXTENSION}" \
